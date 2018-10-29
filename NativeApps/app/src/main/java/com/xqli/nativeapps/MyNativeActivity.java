468967e0faa2ffb1751e1f6910744fb8abda792e
lixuqing <lixuqing2007@126.com>
2018-10-29 23:45:35 +0800

    native code

---
+++ NativeApps/app/src/main/java/com/xqli/nativeapps/MyNativeActivity.java
@@ -0,0 +1,14 @@
+package com.xqli.nativeapps;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MyNativeActivity extends NativeActivity {
+
+    private static final String TAG = "MyNativeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
