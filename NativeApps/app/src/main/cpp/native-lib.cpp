16ea89e0a2100767a71ef5baae469d0defedd873
lixuqing <lixuqing2007@126.com>
2018-10-30 23:55:19 +0800

    native demo

--- NativeApps/app/src/main/cpp/native-lib.cpp
+++ NativeApps/app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,14 @@
 #include <jni.h>
 #include <string>
 
+#include <errno.h>
+
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+
+#include <android/sensor.h>
+#include <android/log.h>
+#include "android_native_app_glue.h"
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
